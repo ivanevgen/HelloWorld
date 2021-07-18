c020674f932f0641519a31b9ca5f856b87ce4eaa
Swedewow <87385983+Swedewow@users.noreply.github.com>
2021-07-18 22:10:06 +0300

    Add files via upload

--- Book.java
+++ Book.java
@@ -3,14 +3,15 @@ public class Book {
         int beerNum = 99;
         String word = "бутылок (бутылки)";
         while (beerNum > 0) {
+            if (beerNum == 1) {
+                word = "бутылка"; // в единственном числе - ОДНА бутылка
+            }
             System.out.println(beerNum + " " + word + " пива на стене");
             System.out.println(beerNum + " " + word + " пива.");
             System.out.println("Возьми одну.");
             System.out.println("Пусти по кругу.");
             beerNum = beerNum - 1;
-            if (beerNum == 1) {
-                word = "бутылка"; // в единственном числе - ОДНА бутылка
-            }
+
 
             if (beerNum > 0) {
                 System.out.println(beerNum + " " + word + " пива на стене");
